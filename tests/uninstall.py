a9cfa56f018a32bd789b10ab447e8352ccd847e9
akakou <s16003@tokyo.kosen-ac.jp>
2019-01-28 17:36:30 +0900

    [add] Uninstallation
    Uninstall scanner or signature.

---
+++ tests/uninstall.py
@@ -0,0 +1,34 @@
+import sys, os, pprint, subprocess
+import unittest
+
+sys.path.append(os.pardir)
+
+from src.func import install
+from src.func import uninstall
+
+
+class TestPackage(unittest.TestCase):
+    def setUp(self):
+        os.system('mkdir .scanners')
+        os.system('mkdir .signatures')
+
+        install.install_scanner('https://github.com/buckler-project/sample-scanner')
+        install.install_signature('https://github.com/buckler-project/sample-signature')
+
+    def tearDown(self):
+        os.system('rm -rf .scanners')
+        os.system('rm -rf .signatures')
+
+    def test_install_scanner(self):
+        uninstall.uninstall_scanner('sample-scanner')
+        files = subprocess.getoutput('ls .scanners')
+        self.assertEqual('', files)
+
+    def test_install_signature(self):
+        uninstall.uninstall_signature('sample-signature')
+        files = subprocess.getoutput('ls .signatures')
+        self.assertEqual('', files)
+
+
+if __name__ == '__main__':
+    unittest.main()
